345e8ce2afd079768743c968551048babb18813e
Sandhiya Nagaraj <sandhiyanagarajnk2909@gmail.com>
2025-12-30 12:47:02 +0530

    DBT- Day: JS conditional pratice

---
+++ day-js-basic.js
@@ -0,0 +1,11 @@
+//mini practice
+
+let mark = 55;
+
+if (mark >= 90) {
+    console.log("Excellent");
+}else if(mark >= 75) {
+    console.log("Very Good");
+}else {
+    console.log("Keep practice");
+}
